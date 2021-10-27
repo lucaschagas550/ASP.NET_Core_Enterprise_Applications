8673766a2e2fccd215fddc0b31df94a117089bda
Lucas Andrade <lucas.chagas@opuspacind.onmicrosoft.com>
2021-10-27 16:55:14 -0300

    Feat: Obtendo dados de usuário do cookie.

--- src/web/NSE.WebApp.MVC/Configuration/DependencyInjectionConfig.cs
+++ src/web/NSE.WebApp.MVC/Configuration/DependencyInjectionConfig.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using NSE.WebApp.MVC.Extensions;
 using NSE.WebApp.MVC.Services;
 
 namespace NSE.WebApp.MVC.Configuration
@@ -9,6 +11,12 @@ namespace NSE.WebApp.MVC.Configuration
         public static void RegisterServices(this IServiceCollection services)
         {
             services.AddHttpClient<IAutenticacaoService, AutenticacaoSerice>();
+
+            //Singleton existe um contexto para apresentação toda
+            services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+
+            //Scoped os dados ficam limitados a cada request 
+            services.AddScoped<IUser, AspNetUser>();
         }
     }
 }
