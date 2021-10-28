a8d413c591fc48bf1611257079502100dab562a2
Lucas Andrade <lucas.chagas@opuspacind.onmicrosoft.com>
2021-10-28 10:20:18 -0300

    Feat: Exibindo os erros para o usuário.

---
+++ src/web/NSE.WebApp.MVC/Extensions/SummaryViewComponet.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace NSE.WebApp.MVC.Extensions
+{
+    public class SummaryViewComponet : ViewComponent
+    {
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            return View();
+        }
+    }
+}
