9dd63cbb5f85bece4cf3ea0c80e98c2df9e988a1
Lucas Andrade <lucas.chagas@opuspacind.onmicrosoft.com>
2021-10-29 15:49:27 -0300

    Feat: Boas práticas com HttpServices.

--- src/web/NSE.WebApp.MVC/Services/AutenticacaoService.cs
+++ src/web/NSE.WebApp.MVC/Services/AutenticacaoService.cs
@@ -1,7 +1,5 @@
 ﻿using NSE.WebApp.MVC.Models;
 using System.Net.Http;
-using System.Text;
-using System.Text.Json;
 using System.Threading.Tasks;
 
 namespace NSE.WebApp.MVC.Services
@@ -18,55 +16,37 @@ namespace NSE.WebApp.MVC.Services
         public async Task<UsuarioRespostaLogin> Login(UsuarioLogin usuarioLogin)
         {
             //Transforma o conteudo em json
-            var loginContent = new StringContent(
-                JsonSerializer.Serialize(usuarioLogin),
-                Encoding.UTF8,
-                "application/json");
+            var loginContent = ObterConteudo(usuarioLogin);
 
             var response = await _httpCliente.PostAsync("https://localhost:44354/api/identidade/autenticar", loginContent);
-
-            //Text.json conflita o retorno entre maiusculas e minusculas, exemplo: AccessToken do model com  json está accessToken, está config resolve isso
-            var options = new JsonSerializerOptions
-            {
-                PropertyNameCaseInsensitive = true,
-            };
 
             if (!TratarErrosResponse(response))
             {
                 return new UsuarioRespostaLogin
                 {
-                    ResponseResult = JsonSerializer.Deserialize<ResponseResult>(await response.Content.ReadAsStringAsync(), options)
+                    ResponseResult = await DeserializarObjetoResponse<ResponseResult>(response)
                 };
             }
 
-            return JsonSerializer.Deserialize<UsuarioRespostaLogin>(await response.Content.ReadAsStringAsync(), options);
+            return await DeserializarObjetoResponse<UsuarioRespostaLogin>(response);
         }
 
         public async Task<UsuarioRespostaLogin> Registro(UsuarioRegistro usuarioRegistro)
         {
             //Transforma o conteudo em json
-            var registroContent = new StringContent(
-                JsonSerializer.Serialize(usuarioRegistro),
-                Encoding.UTF8,
-                "application/json");
+            var registroContent = ObterConteudo(usuarioRegistro);
 
             var response = await _httpCliente.PostAsync("https://localhost:44354/api/identidade/nova-conta", registroContent);
-
-            //Text.json conflita o retorno entre maiusculas e minusculas, exemplo: AccessToken do model com  json está accessToken, está config resolve isso
-            var options = new JsonSerializerOptions
-            {
-                PropertyNameCaseInsensitive = true,
-            };
 
             if (!TratarErrosResponse(response))
             {
                 return new UsuarioRespostaLogin
                 {
-                    ResponseResult = JsonSerializer.Deserialize<ResponseResult>(await response.Content.ReadAsStringAsync(), options)
+                    ResponseResult = await DeserializarObjetoResponse<ResponseResult>(response)
                 };
             }
 
-            return JsonSerializer.Deserialize<UsuarioRespostaLogin>(await response.Content.ReadAsStringAsync(), options);
+            return await DeserializarObjetoResponse<UsuarioRespostaLogin>(response);
         }
     }
 }
