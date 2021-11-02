2f238a5471aaf08d747b5b657bc7c151d8c83453
Lucas Andrade <lucas.chagas@opuspacind.onmicrosoft.com>
2021-11-02 15:48:08 -0300

    Feat: Modelagem API Catalago.

---
+++ src/building blocks/NSE.Core/DomainObjects/Entity.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace NSE.Core.DomainObjects
+{
+
+    //entidade que gera o ID para todos os objetos já que todos são obrigados a ter um ID
+    public abstract class Entity
+    {
+        public Guid Id { get; set; }
+
+
+        //Compara se o id de um produto eh igual ou diferente de outro
+        public override bool Equals(object obj)
+        {
+            var compareTo = obj as Entity;
+
+            if (ReferenceEquals(this, compareTo)) return true;
+            if (ReferenceEquals(null, compareTo)) return false;
+
+            return Id.Equals(compareTo.Id);
+        }
+
+        public static bool operator ==(Entity a, Entity b)
+        {
+            if (ReferenceEquals(a, null) && ReferenceEquals(b, null))
+                return true;
+
+            if (ReferenceEquals(a, null) || ReferenceEquals(b, null))
+                return false;
+
+            return a.Equals(b);
+        }
+
+        public static bool operator !=(Entity a, Entity b)
+        {
+            return !(a == b);
+        }
+
+        public override int GetHashCode()
+        {
+            return (GetType().GetHashCode() * 907) + Id.GetHashCode();
+        }
+
+        public override string ToString()
+        {
+            return $"{GetType().Name} [Id={Id}]";
+        }
+    }
+}
