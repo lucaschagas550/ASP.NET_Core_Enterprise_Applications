20b1e4ccf2def818ace31bb4fb1d1d0797f99a0c
Lucas Andrade <lucasandarde595@gmail.com>
2021-11-04 08:36:28 -0300

    Feat: Comportamento básicos API Catalogo.

---
+++ src/services/NSE.Catalogo.API/Configuration/ApiConfig.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using NSE.Catalogo.API.Data;
+using Microsoft.AspNetCore.Hosting;
+
+namespace NSE.Catalogo.API.Configuration
+{
+    public static class ApiConfig
+    {
+        public static void AddApiConfiguration(this IServiceCollection services, IConfiguration configuration)
+        {
+            //Configuração de conexão com banco de dados
+            services.AddDbContext<CatalogoContext>(options =>
+                options.UseMySql(configuration.GetConnectionString("DefaultConnection")));
+
+            services.AddControllers();
+
+            services.AddCors(options =>
+            {
+                options.AddPolicy("Total",
+                builder => 
+                    builder.AllowAnyOrigin()
+                           .AllowAnyMethod()
+                           .AllowAnyHeader());
+            });
+        }
+
+        public static void UseApiConfiguration(this IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseCors("Total");
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
---
+++ src/services/NSE.Catalogo.API/Controllers/CatalogoController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NSE.Catalogo.API.Models;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace NSE.Catalogo.API.Controllers
+{
+    public class CatalogoController : Controller
+    {
+        private readonly IProdutoRepository _produtoRepository;
+
+        public CatalogoController(IProdutoRepository produtoRepository)
+        {
+            _produtoRepository = produtoRepository;
+        }
+
+        [HttpGet("catalogo/produtos")]
+        public async Task<IEnumerable<Produto>> Index()
+        {
+            return await _produtoRepository.ObterTodos(); 
+        } 
+        
+
+        //Pela entidade ser simples retorno a entidade completa PRODUTO
+        //Mas se a entiddade for complexa o ideal eh retornar um objeto response que represente somente os dados q eu quero retornar
+        [HttpGet("catalogo/produtos/{id}")]
+        public async Task<Produto> Index(Guid id)
+        {
+            return await _produtoRepository.ObterPorId(id); 
+        }
+    }
+}
