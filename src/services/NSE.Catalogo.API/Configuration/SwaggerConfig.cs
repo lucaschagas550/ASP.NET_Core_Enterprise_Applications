39fed1fb10f547e3bb461d3abc7692a4349a375c
Lucas Andrade <lucasandarde595@gmail.com>
2021-11-08 08:23:37 -0300

    Feat: Validação de acesso baseado em claims.

--- src/services/NSE.Catalogo.API/Configuration/SwaggerConfig.cs
+++ src/services/NSE.Catalogo.API/Configuration/SwaggerConfig.cs
@@ -19,8 +19,33 @@ namespace NSE.Catalogo.API.Configuration
                     Contact = new OpenApiContact() { Name = "Lucas Andrade", Email = "lucasandrade595@gmail.com" },
                     License = new OpenApiLicense() { Name = "MIT", Url = new Uri("https://opensource.org/licenses/MIT") }
                 });
-            });
 
+                //Autenticação do JWT para Swagger
+                c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+                {
+                    Description = "Insira o token JWT desta maneira: Bearer {seu token}",
+                    Name = "Authorization",
+                    Scheme = "Bearer",
+                    BearerFormat = "JWT",
+                    In = ParameterLocation.Header,
+                    Type = SecuritySchemeType.ApiKey
+                });
+
+                c.AddSecurityRequirement(new OpenApiSecurityRequirement
+                {
+                    {
+                        new OpenApiSecurityScheme
+                        {
+                            Reference = new OpenApiReference
+                            {
+                                Type = ReferenceType.SecurityScheme,
+                                Id = "Bearer"
+                            }
+                        },
+                        new string[] {}
+                    }
+                });
+            });
         }
 
         public static void UseSwaggerConfiguration(this IApplicationBuilder app)
--- src/services/NSE.Catalogo.API/Controllers/CatalogoController.cs
+++ src/services/NSE.Catalogo.API/Controllers/CatalogoController.cs
@@ -1,11 +1,15 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using NSE.Catalogo.API.Models;
 using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
+using static NSE.WebAPI.Core.Identidade.CustomAuthorization;
 
 namespace NSE.Catalogo.API.Controllers
 {
+    [ApiController]
+    [Authorize]
     public class CatalogoController : Controller
     {
         private readonly IProdutoRepository _produtoRepository;
@@ -15,6 +19,7 @@ namespace NSE.Catalogo.API.Controllers
             _produtoRepository = produtoRepository;
         }
 
+        [AllowAnonymous]
         [HttpGet("catalogo/produtos")]
         public async Task<IEnumerable<Produto>> Index()
         {
@@ -24,6 +29,7 @@ namespace NSE.Catalogo.API.Controllers
 
         //Pela entidade ser simples retorno a entidade completa PRODUTO
         //Mas se a entiddade for complexa o ideal eh retornar um objeto response que represente somente os dados q eu quero retornar
+        [ClaimsAuthorize("Catalogo", "Ler")]
         [HttpGet("catalogo/produtos/{id}")]
         public async Task<Produto> Index(Guid id)
         {
