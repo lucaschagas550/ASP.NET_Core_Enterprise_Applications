9c97d506f03e84c47ec129e751e2373fe86a1aff
Lucas Andrade <lucasandarde595@gmail.com>
2021-11-10 09:00:00 -0300

    Feat: Entendendo os DelegatingHandlers.

--- src/web/NSE.WebApp.MVC/Configuration/DependencyInjectionConfig.cs
+++ src/web/NSE.WebApp.MVC/Configuration/DependencyInjectionConfig.cs
@@ -2,6 +2,7 @@
 using Microsoft.Extensions.DependencyInjection;
 using NSE.WebApp.MVC.Extensions;
 using NSE.WebApp.MVC.Services;
+using NSE.WebApp.MVC.Services.Handlers;
 
 namespace NSE.WebApp.MVC.Configuration
 {
@@ -10,9 +11,14 @@ namespace NSE.WebApp.MVC.Configuration
 
         public static void RegisterServices(this IServiceCollection services)
         {
+            //Transient => é chamado uma instancia cada vez
+            services.AddTransient<HttpClientAuthorizationDelegatingHandler>();
+
             services.AddHttpClient<IAutenticacaoService, AutenticacaoService>();
 
-            services.AddHttpClient<ICatalogoService, CatalogoService>();
+            //Handler manipula os request, interceptando todos os request que vier de CatalogoService
+            services.AddHttpClient<ICatalogoService, CatalogoService>()
+                    .AddHttpMessageHandler<HttpClientAuthorizationDelegatingHandler>();
 
             //Singleton existe um contexto para apresentação toda
             services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
---
+++ src/web/NSE.WebApp.MVC/Services/Handlers/HttpClientAuthorizationDelegatingHandler.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading;
+using System.Threading.Tasks;
+using NSE.WebApp.MVC.Extensions;
+namespace NSE.WebApp.MVC.Services.Handlers
+{
+    public class HttpClientAuthorizationDelegatingHandler : DelegatingHandler
+    {
+        private readonly IUser _user;
+
+        public HttpClientAuthorizationDelegatingHandler(IUser user)
+        {
+            _user = user;
+        }
+
+        //SobreEscreve o sendAsync do seu request e adiciona o token de autorizãcao no header
+        protected override Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+        {
+            var authorizationHeader = _user.ObterHttpContext().Request.Headers["Authorization"];
+
+            if (!string.IsNullOrEmpty(authorizationHeader))
+            {
+                request.Headers.Add("Authorization", new List<string>() { authorizationHeader });
+            }
+
+            var token = _user.ObterUserToken();
+
+            if (token != null)
+            {
+                request.Headers.Authorization = new AuthenticationHeaderValue("Bearer", token);
+            }
+
+            return base.SendAsync(request, cancellationToken);
+        }
+    }
+}
