8eb13beaf2f9ece94da9ca6214a8ee1af76476a4
Lucas Andrade <lucasandarde595@gmail.com>
2021-11-18 07:49:01 -0300

    Feat: Definindo as entidades.

---
+++ src/services/NSE.Cliente.API/Models/Clientess.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using NSE.Core.DomainObjects;
+
+namespace NSE.Cliente.API.Models
+{
+    public class Clientess : Entity, IAggregateRoot
+    {
+        public string Nome { get; private set; }
+        public Email Email { get; private set; }
+        public Cpf Cpf { get; private set; }
+        public bool Excluido { get; private set; }
+        public Endereco Endereco { get; private set; }
+
+        // EF Relation ele herda da classe
+        protected Clientess() { }
+
+        public Clientess(Guid id, string nome, string email, string cpf)
+        {
+            Id = id;
+            Nome = nome;
+            Email = new Email(email);
+            Cpf = new Cpf(cpf);
+            Excluido = false;
+        }
+
+        public void TrocarEmail(string email)
+        {
+            Email = new Email(email);
+        }
+
+        public void AtribuirEndereco(Endereco endereco)
+        {
+            Endereco = endereco;
+        }
+    }
+}
