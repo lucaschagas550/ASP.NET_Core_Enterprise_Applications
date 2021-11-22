a43981dfb1a9dce965abe1780f045b7ad3ed26eb
Lucas Andrade <lucasandarde595@gmail.com>
2021-11-22 07:32:34 -0300

    Feat: Mapeamento das tabelas.

---
+++ src/services/NSE.Cliente.API/Data/Mappings/ClienteMapping.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using NSE.Cliente.API.Models;
+using NSE.Core.DomainObjects;
+
+namespace NSE.Cliente.API.Data.Mappings
+{
+    public class ClienteMapping : IEntityTypeConfiguration<Clientess>
+    {
+        public void Configure(EntityTypeBuilder<Clientess> builder)
+        {
+            builder.HasKey(c => c.Id);
+
+            builder.Property(c => c.Nome)
+                .IsRequired()
+                .HasColumnType("varchar(200)");
+
+            //OwnsOne: Configura uma relação na qual a entidade de destino pertence (ou parte dela) essa entidade.
+            builder.OwnsOne(c => c.Cpf, tf =>
+            {
+                tf.Property(c => c.Numero)
+                    .IsRequired()
+                    .HasMaxLength(Cpf.CpfMaxLength)
+                    .HasColumnName("Cpf")
+                    .HasColumnType($"varchar({Cpf.CpfMaxLength})");
+            });
+
+            builder.OwnsOne(c => c.Email, tf =>
+            {
+                tf.Property(c => c.Endereco)
+                    .IsRequired()
+                    .HasColumnName("Email")
+                    .HasColumnType($"varchar({Email.EnderecoMaxLength})");
+            });
+
+            // 1 : 1 => Aluno : Endereco
+            builder.HasOne(c => c.Endereco)
+                .WithOne(c => c.Cliente);
+
+            builder.ToTable("Clientes");
+        }
+    }
+}
---
+++ src/services/NSE.Cliente.API/Data/Repository/ClienteRepository.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using NSE.Cliente.API.Models;
+using NSE.Core.Data;
+
+namespace NSE.Cliente.API.Data.Repository
+{
+    public class ClienteRepository : IClienteRepository
+    {
+        private readonly ClientesContext _context;
+
+        public ClienteRepository(ClientesContext context)
+        {
+            _context = context;
+        }
+
+        public IUnitOfWork UnitOfWork => _context;
+
+        public async Task<IEnumerable<Clientess>> ObterTodos()
+        {
+            return await _context.Clientes.AsNoTracking().ToListAsync();
+        }
+
+        public Task<Clientess> ObterPorCpf(string cpf)
+        {
+            return _context.Clientes.FirstOrDefaultAsync(c => c.Cpf.Numero == cpf);
+        }
+
+        public void Adicionar(Clientess cliente)
+        {
+            _context.Clientes.Add(cliente);
+        }
+
+        public void Dispose()
+        {
+            _context.Dispose();
+        }
+    }
+}
