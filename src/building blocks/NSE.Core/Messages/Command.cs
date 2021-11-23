ef5c361fe5d1bc2dcc7a8ea909f86bd7d1475b8b
Lucas Andrade <lucasandarde595@gmail.com>
2021-11-23 08:37:38 -0300

    Feat: Command e Command Handler

---
+++ src/building blocks/NSE.Core/Messages/Command.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using FluentValidation.Results;
+using MediatR;
+
+namespace NSE.Core.Messages
+{
+    public abstract class Command : Message, IRequest<ValidationResult>
+    {
+        public DateTime Timestamp { get; private set; }
+
+        //Do pacote FluentValidation, possui uma lista de erros
+        public ValidationResult ValidationResult { get; set; }
+
+        protected Command()
+        {
+            Timestamp = DateTime.Now;
+        }
+
+        public virtual bool EhValido()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
