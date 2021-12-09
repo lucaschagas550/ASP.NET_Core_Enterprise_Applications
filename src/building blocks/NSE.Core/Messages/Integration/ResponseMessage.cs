786a90f21b07207f4a9e8c1f58cf22e6b9a5cd5a
DESKTOP-6U832JM\Lucas Chagas <lucasandrade595@gmail.com>
2021-12-09 15:56:49 -0300

    Feat: Request Response Pattern.

---
+++ src/building blocks/NSE.Core/Messages/Integration/ResponseMessage.cs
@@ -0,0 +1,19 @@
+﻿using FluentValidation.Results;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NSE.Core.Messages.Integration
+{
+    //Resposta da Api
+    public class ResponseMessage : Message
+    {
+        //Já possui metodos de validação
+        public ValidationResult ValidationResult { get; set; }
+
+        public ResponseMessage(ValidationResult validationResult)
+        {
+            ValidationResult = validationResult;
+        }
+    }
+}
